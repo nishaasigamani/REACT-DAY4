2ffd1945cf49ceb98e1e275bddc0b1a11a71d17a
nishaasigamani <108520824+nishaasigamani@users.noreply.github.com>
2023-04-04 22:50:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+import FruitSurvey from './Components/Fruit';
+function App(){
+  return (
+    <div>
+      <FruitSurvey />
+    </div>
+  );
+}
+export default App;
